7e65362126b76c1e2ff55b56ecfc6d2121fd795c
Mert <merttdem@gmail.com>
2020-07-17 18:29:52 +0300

    init

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,10 @@
-import assetManager from './assetManager';
-import Confetti from '../utils/confetti';
-import Ui from './ui';
-import Level from './level';
+import assetManager from "./assetManager";
+import Confetti from "../utils/confetti";
+import Ui from "./ui";
+import Level from "./level";
+import {Body, Sphere, Plane, Box, Vec3} from "cannon";
+import {Vector3, Box3} from "three";
+import {BrotliDecompressedSize} from "../brotli/unbrotli";
 
 var gameEnded = false;
 var main, clock, controls, ui;
@@ -9,260 +12,386 @@ var isTest = true;
 var data, confettiMaker;
 var updateFunction;
 
-class Game{
-    constructor(_main){
+class Game {
+  constructor(_main) {}
 
+  boot(_main) {
+    main = _main;
+    main.data = app.data;
+    data = app.data;
+    main.isTest = isTest;
+
+    main.renderer.outputEncoding = THREE.GammaEncoding;
+    main.renderer.gammaFactor = 2.2;
+
+    ////add fog here if you want fog
+
+    clock = new THREE.Clock();
+
+    if (app.type == "tapjoy" && window.TJ_API) {
+      window.TJ_API.setPlayableBuild("v1.0");
+      window.TJ_API.setPlayableAPI({
+        skipAd: function () {
+          app.data.hasTryAgain = false;
+          this.endGame(false);
+        },
+      });
     }
 
-    boot(_main){
-        main = _main;
-        main.data = app.data;
-        data = app.data;
-        main.isTest = isTest;
+    assetManager.loadAssets(main, (list) => {
+      this.assetList = list;
+      main.assets = list;
+      main.assetsLoaded();
+    });
+  }
 
-        main.renderer.outputEncoding  = THREE.GammaEncoding;
-        main.renderer.gammaFactor = 2.2;
+  init(fromRestart) {
+    confettiMaker = new Confetti(main.scene);
+    this.initLights();
+    this.initControls();
 
-        ////add fog here if you want fog
+    this.cam = main.camera;
+    this.cam.position.set(0, 20, -15);
+    this.cam.lookAt(0, 0, 0);
 
-        clock = new THREE.Clock();
+    main.initCannonDebug();
+    main.world.allowSleep = false;
 
-        if(app.type == "tapjoy" && window.TJ_API){
-            window.TJ_API.setPlayableBuild("v1.0");
-            window.TJ_API.setPlayableAPI({
-                skipAd: function() {
-                    app.data.hasTryAgain = false;
-                    this.endGame(false);
-                }
-            })
-        }
-    
-        assetManager.loadAssets(main,(list)=>{
-            this.assetList = list;
-            main.assets = list;
-            main.assetsLoaded();
-            
+    // this.level = new Level();
+    // this.level.start();
+
+    // / / /     C O D E   B E L O W     \ \ \ \\
+
+    let red = 0xff0000;
+    let cyan = 0x68f0f0;
+    let gray = 0x393537;
+    let rows = 12;
+    let columns = 4;
+
+    // Add red path
+
+    let pathGeo = new THREE.BoxGeometry(10, 2, 150);
+    let pathMat = new THREE.MeshBasicMaterial({
+      color: red,
+    });
+    this.path = new THREE.Mesh(pathGeo, pathMat);
+
+    this.path.position.set(0, 0, 0);
+    main.scene.add(this.path);
+    this.path.body = new Body({
+      position: this.path.position,
+      mass: 0,
+    });
+    let pathShape = new Box(new Vec3(5, 1, 25)); //cannonjs
+    this.path.body.addShape(pathShape);
+    main.world.add(this.path.body);
+    //console.log(this.path);
+
+    // Gets path position x,y,z
+
+    let pathVec = new Vector3(); // threejs
+    let path3 = new Box3().setFromObject(this.path);
+    let pathSize = path3.getSize(pathVec);
+
+    // Add cyan platform
+
+    let plat = new Vec3(pathSize.x * 0.8, 1, 1.5);
+    let platGeo = new THREE.BoxGeometry(plat.x, plat.y, plat.z);
+    let platMat = new THREE.MeshPhongMaterial({
+      color: cyan,
+    });
+
+    this.platform = new THREE.Mesh(platGeo, platMat);
+    this.platform.position.set(0, 2, 5);
+    main.scene.add(this.platform);
+
+    this.platform.body = new Body({
+      position: this.platform.position,
+      mass: 1,
+    });
+
+    let platformShape = new Box(plat.mult(0.5));
+    this.platform.body.addShape(platformShape);
+    main.world.add(this.platform.body);
+
+    // Add cyan ball
+
+    let ballGeo = new THREE.SphereGeometry(0.7, 50, 50);
+    let ballMat = new THREE.MeshPhongMaterial({
+      color: cyan,
+    });
+    this.ball = new THREE.Mesh(ballGeo, ballMat);
+
+    this.ball.position.set(0, 2, -5);
+    main.scene.add(this.ball);
+
+    this.ball.body = new Body({
+      position: this.ball.position,
+      mass: 30,
+    });
+    let ballShape = new Sphere(0.7);
+    this.ball.body.addShape(ballShape);
+    main.world.add(this.ball.body);
+
+    //console.log(this.ball);
+
+    // Add gray boxes
+
+    let boxGeo = new THREE.BoxGeometry(1, 1, 1);
+    let boxMat = new THREE.MeshPhongMaterial({
+      color: gray,
+    });
+    this.box = new THREE.Mesh(boxGeo, boxMat);
+
+    this.boxList = [];
+
+    // Gets box position x,y,z
+
+    let boxVec = new Vector3();
+    let box3 = new Box3().setFromObject(this.box);
+    let boxSize = box3.getSize(boxVec);
+
+    // Placing boxes on the platform
+
+    for (let i = 0; i < columns; i++) {
+      for (let j = 0; j < rows; j++) {
+        let x = 3.7 - i * (boxSize.x * 2.5);
+        let z = 10 + j * (boxSize.z * 2.5);
+        let y = 2;
+
+        boxGeo = new THREE.BoxGeometry(1.2, 1.2, 1.2);
+        boxMat = new THREE.MeshPhongMaterial({
+          color: gray,
         });
 
+        this.box = new THREE.Mesh(boxGeo, boxMat);
+        this.box.position.set(x, y, z);
+        main.scene.add(this.box);
+
+        this.box.body = new Body({
+          position: this.box.position,
+          mass: 1,
+        });
+        let boxShape = new Box(new Vec3(0.6, 0.6, 0.6));
+        this.box.body.addShape(boxShape);
+        main.world.add(this.box.body);
+
+        this.boxList.push(this.box);
+      }
+    }
+    console.log(this.boxList);
+
+    if (fromRestart) {
+      return;
     }
 
-    init(fromRestart){
-        confettiMaker = new Confetti(main.scene);
-        this.initLights();
-        this.initControls();
+    // window.main = main;
+    // window.THREE = THREE;
 
-        let cam = main.camera;
-        cam.position.set(0,20,20);
-        cam.lookAt(0,0,0);
+    // Call these functions once
 
-        main.initCannonDebug();
-        main.world.allowSleep = true;
-        
-        this.level = new Level();
-        this.level.start();
-        
-        if(fromRestart){
-            return;
+    updateFunction = this.update.bind(this);
+    this.initUi();
+    this.update();
+    this.initTouchEvents();
+
+    // this.onResizeCallback(main.lastWidth, main.lastHeight);
+    // setTimeout(() => {
+    //   this.onResizeCallback(main.lastWidth, main.lastHeight);
+    // }, 500);
+  }
+
+  initControls() {
+    controls = main.utility.initControls();
+    //controls = main.utility.initControlsPointer(); ///for pepjs
+    app.controls = controls;
+
+    if (isTest) {
+      isTest = false;
+      window.onkeydown = function (e) {
+        if (e.key == "a" && !isTest) {
+          isTest = true;
+          controls = main.utility.startOrbitControls(0, 1500);
         }
 
-        ////call these functions once
-        updateFunction = this.update.bind(this);
-        this.initUi();
-        this.update();    
-        this.initTouchEvents();
-
-
-        this.onResizeCallback(main.lastWidth, main.lastHeight);
-        setTimeout(()=>{
-            this.onResizeCallback(main.lastWidth, main.lastHeight);
-        },500);
-
-        
-
-        
-
-    }
-
-    initControls(){
-        controls = main.utility.initControls();
-        //controls = main.utility.initControlsPointer(); ///for pepjs
-        app.controls = controls;
-
-        if(isTest){
-            isTest = false;
-            window.onkeydown = function(e){
-                if(e.key == "a" && !isTest){
-                    isTest = true;
-                    controls = main.utility.startOrbitControls(0,1500);
-                }
-        
-                if(e.key == "s" && isTest){
-                    isTest = false;
-                    controls.dispose && controls.dispose();
-                    
-                }
-                main.isTest = isTest;
-            }
+        if (e.key == "s" && isTest) {
+          isTest = false;
+          controls.dispose && controls.dispose();
         }
+        main.isTest = isTest;
+      };
+    }
+  }
+
+  initUi() {
+    let uiDiv = document.getElementById("ui");
+    ui = new Ui(uiDiv);
+    ui.prepare();
+  }
+
+  update() {
+    main.update();
+    requestAnimationFrame(updateFunction);
+
+    var delta = clock.getDelta();
+    if (!delta || isNaN(delta)) delta = 0.01;
+    if (delta > 0.03) delta = 0.03;
+    var ratio = delta * 60;
+
+    this.ball.position.copy(this.ball.body.position);
+    this.ball.quaternion.copy(this.ball.body.quaternion);
+
+    this.platform.position.copy(this.platform.body.position);
+    this.platform.quaternion.copy(this.platform.body.quaternion);
+
+    this.path.position.copy(this.path.body.position);
+    this.path.quaternion.copy(this.path.body.quaternion);
+
+    for (let boxes of this.boxList) {
+      boxes.position.copy(boxes.body.position);
+      boxes.quaternion.copy(boxes.body.quaternion);
     }
 
-    initUi(){
-        let uiDiv= document.getElementById("ui");
-        ui = new Ui(uiDiv);
-        ui.prepare();
+    let controls = app.controls;
+
+    if (controls.isDown) {
+      console.log("mert");
+      console.log(this.ball.position);
+      let dx = controls.prevX - controls.mouseX;
+      let dz = 1;
+
+      dx *= 0.1;
+      dz *= 0.5;
+
+      this.ball.body.velocity.x += dx;
+      this.ball.body.velocity.z += dz;
+      this.cam.position.z += dz / 4;
     }
 
-    update(){
-        main.update();    
-        requestAnimationFrame(updateFunction);
+    // this.level.update(ratio, delta);
 
-        var delta = clock.getDelta();
-        if(!delta || isNaN(delta))delta = 0.01;
-        if(delta > 0.03)delta = 0.03;
-        var ratio = delta*60;
+    confettiMaker && confettiMaker.update();
 
-        this.level.update(ratio, delta);
+    main.CANNON && main.CANNON.cannonDebugRenderer && main.CANNON.cannonDebugRenderer.update();
+    main.world && main.world.step(delta);
+    main.renderer.render(main.scene, main.camera);
+  }
 
-        confettiMaker && confettiMaker.update();
+  onResizeCallback() {
+    if (!main) {
+      return;
+    }
+    let w = window.innerWidth;
+    let h = window.innerHeight;
 
-        main.CANNON && main.CANNON.cannonDebugRenderer && main.CANNON.cannonDebugRenderer.update();
-        main.world && main.world.step(delta);    
-        main.renderer.render(main.scene, main.camera);
+    app.w = w;
+    app.h = h;
+
+    if (ui) {
+      ui.resize(w, h);
+    }
+  }
+
+  initLights() {
+    let lightColor = 0xffffff;
+
+    let ambientLight = new THREE.AmbientLight(lightColor, 0.2); //0.7
+    main.scene.add(ambientLight);
+
+    var dirLight = new THREE.DirectionalLight(0xffffff, 0.9);
+    dirLight.position.set(-300, 1000, 0);
+    main.scene.add(dirLight);
+  }
+
+  initTouchEvents() {
+    function pointerDown(e) {
+      let install = ui.install;
+      let bottomBanner = ui.bottomBanner;
+
+      if (install && !install.classList.contains("show")) {
+        install.classList.add("show");
+        install.resize && install.resize();
+      }
+
+      if (bottomBanner && !bottomBanner.classList.contains("show")) {
+        bottomBanner.classList.add("show");
+        bottomBanner.resize && bottomBanner.resize();
+      }
+
+      if (!main.timeStarted && app.type != "mobvista") {
+        main.startTimer();
+      }
     }
 
-    onResizeCallback(){
-        if(!main){
-            return;
-        }
-        let w = window.innerWidth;
-        let h = window.innerHeight;
-        
-        app.w = w;
-        app.h = h;
-    
-        if(ui){
-            ui.resize(w, h);
-        }
-    }
+    function pointerUp(e) {}
 
-    initLights(){
-        let lightColor = 0xffffff;
-        
-        let ambientLight = new THREE.AmbientLight(lightColor, 1);//0.7
-        main.scene.add(ambientLight);
+    let domElement = document.body;
 
-        var dirLight = new THREE.DirectionalLight( 0xffffff, 0.9 );
-        dirLight.position.set(-300, 1000, 0 );
-        main.scene.add( dirLight );
-    }
-
-    initTouchEvents(){
-    
-        function pointerDown(e){
-    
-            let install = ui.install;
-            let bottomBanner = ui.bottomBanner;
-            
-            if(install && !install.classList.contains("show")){
-                install.classList.add("show");
-                install.resize && install.resize();
-            }
-
-            if(bottomBanner && !bottomBanner.classList.contains("show")){
-                bottomBanner.classList.add("show");
-                bottomBanner.resize && bottomBanner.resize();
-            }
-    
-            if(!main.timeStarted && app.type != "mobvista"){
-                main.startTimer();
-            }
-    
-            
-        }
-    
-        function pointerUp(e){
-            
-        }
-        
-    
-        
-        let domElement = document.body;
-        
-        ///pointer events require pepjs on safari
-        /*domElement.addEventListener("pointerdown", pointerDown);
+    ///pointer events require pepjs on safari
+    /*domElement.addEventListener("pointerdown", pointerDown);
         domElement.addEventListener("pointerup", pointerUp);
         domElement.addEventListener("pointermove", pointerMove);*/
-    
-        domElement.addEventListener("touchstart", pointerDown);
-        domElement.addEventListener("touchend", pointerUp);
-    
-        if ('ontouchstart' in document.documentElement) {
-        }
-        else{
-            domElement.addEventListener("mousedown", pointerDown);
-            domElement.addEventListener("mouseup", pointerUp);
-        }
-    
-        
+
+    domElement.addEventListener("touchstart", pointerDown);
+    domElement.addEventListener("touchend", pointerUp);
+
+    if ("ontouchstart" in document.documentElement) {
+    } else {
+      domElement.addEventListener("mousedown", pointerDown);
+      domElement.addEventListener("mouseup", pointerUp);
+    }
+  }
+
+  endGame(didWon, reason) {
+    if (gameEnded) {
+      return;
     }
 
-    endGame(didWon, reason){
-        if(gameEnded){
-            return;
-        }
-        
-        gameEnded = true;
-        main.gameEnded = true;
-        
-        
-        ui.addEndCard();
-        ////Call this part if the game is completely ended
+    gameEnded = true;
+    main.gameEnded = true;
 
-        if(app.type == "tapjoy" && window.TJ_API){
-    
-            if(didWon){
-                window.TJ_API && window.TJ_API.objectiveComplete();
-            }
-            window.TJ_API && window.TJ_API.gameplayFinished();
-            
-            if (window.TJ_API && window.TJ_API.directives.showEndCard) {
-                // render end card
-            } else { 
-                /* prepare for Tapjoy endcard */
-                return;
-            }
-        }
-        
-        main.gameFinished(didWon, reason);
+    ui.addEndCard();
+    ////Call this part if the game is completely ended
+
+    if (app.type == "tapjoy" && window.TJ_API) {
+      if (didWon) {
+        window.TJ_API && window.TJ_API.objectiveComplete();
+      }
+      window.TJ_API && window.TJ_API.gameplayFinished();
+
+      if (window.TJ_API && window.TJ_API.directives.showEndCard) {
+        // render end card
+      } else {
+        /* prepare for Tapjoy endcard */
+        return;
+      }
     }
 
-    restartGame(){
-    
-        main.restartGame( data.totalTime );
-        
-        gameEnded = false;
-        main.gameEnded = false;
-        let scene = main.scene;
-    
-        for( var i = scene.children.length - 1; i >= 0; i--) {
-            let obj = scene.children[i];
-            scene.remove(obj);
-            if(obj.body){
-                if(main.CANNON){
-                    main.world.remove(obj.body);
-                }
-                else if(main.matter){
-                    main.mater.removeBody(obj.body);
-                }
-                
-            }
+    main.gameFinished(didWon, reason);
+  }
+
+  restartGame() {
+    main.restartGame(data.totalTime);
+
+    gameEnded = false;
+    main.gameEnded = false;
+    let scene = main.scene;
+
+    for (var i = scene.children.length - 1; i >= 0; i--) {
+      let obj = scene.children[i];
+      scene.remove(obj);
+      if (obj.body) {
+        if (main.CANNON) {
+          main.world.remove(obj.body);
+        } else if (main.matter) {
+          main.mater.removeBody(obj.body);
         }
-    
-        main.objectMaker.clear();
-        this.init(true);
-        
+      }
     }
 
+    main.objectMaker.clear();
+    this.init(true);
+  }
 }
 
 export default Game;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import "./css/style_core.css";
 import "./css/gear.css";
 import "./css/style.css";
@@ -7,115 +6,107 @@ import "./css/style.css";
 ///sound
 //import {Howl, Howler} from 'howler';
 
-var THREE = require('three');
-
+var THREE = require("three");
 
 window.THREE = THREE;
 
-require('./utils/AnimUtilsFix');
+require("./utils/AnimUtilsFix");
 
-require('three/examples/js/loaders/GLTFLoader');
+require("three/examples/js/loaders/GLTFLoader");
 //require('./utils/GPUParticleSystem');
-require('three/examples/js/controls/OrbitControls');
-require('three/examples/js/utils/SkeletonUtils');
+require("three/examples/js/controls/OrbitControls");
+require("three/examples/js/utils/SkeletonUtils");
 
-require('three-ziploader');
-require('./utils/TrailRenderer');
+require("three-ziploader");
+require("./utils/TrailRenderer");
 
 //import Main from './three_main';
-import Main from './threecannon_main';
+import Main from "./threecannon_main";
 
-import Game from './game/game';
+import Game from "./game/game";
 var game = new Game();
 
-var disableBodyScroll = require('body-scroll-lock').disableBodyScroll;
+var disableBodyScroll = require("body-scroll-lock").disableBodyScroll;
 
 var app = {disableBodyScroll};
 window.app = app;
 
 app.type = window.type;
-if(!app.type){
-    app.type=null;
+if (!app.type) {
+  app.type = null;
 }
-
 
 var data = {
-    ///banner details
-    hasBanner: true,
-    bannerText:"Stickman Dash!",
-    bannerTextColor:"0xffffff",
-    bannerBgColor:"0x000000",
-    bannerOpacity:1,
-    
-    hasBottomBanner: true,
-    bottomBannerNo: 0,
-    bottomBannerOpacity: 1,
+  ///banner details
+  hasBanner: true,
+  bannerText: "Stickman Dash!",
+  bannerTextColor: "0xffffff",
+  bannerBgColor: "0x000000",
+  bannerOpacity: 1,
 
-    hasTutorial: false,
-    tutorialText: "Hold & Drag!",
+  hasBottomBanner: true,
+  bottomBannerNo: 0,
+  bottomBannerOpacity: 1,
 
-    hasTryAgain:true,
-    numOfChances:1,
-    
-    ////End card
-    endCardNo: 0,
-    endCardFullScreenClick: true,
-    
-    gameButtonText:"Play Now!",//
-    gameButtonColor: "#ff0000",//
-    gameButtonTextColor: "#ffffff",//
-    gameButtonAnimName:"pulse",//
-    
-    bgColor:"#4B75FF",//4B75FF
+  hasTutorial: false,
+  tutorialText: "Hold & Drag!",
 
-    hasGradient:false,
-    topGradient:"#4B75FF",
-    botGradient:"#ffffff",//87CFE6
-    
-    
-    resolution: 2,
-    totalTime: 26,
-    hasSound:false,
-    
-    gotoMarketClickNum:0,///
-    gotoMarketAfterTime:0,///
+  hasTryAgain: true,
+  numOfChances: 1,
 
-    gotoMarketOnSecondLevel: false,
+  ////End card
+  endCardNo: 0,
+  endCardFullScreenClick: true,
+
+  gameButtonText: "Play Now!", //
+  gameButtonColor: "#ff0000", //
+  gameButtonTextColor: "#ffffff", //
+  gameButtonAnimName: "pulse", //
+
+  bgColor: "#4B75FF", //4B75FF
+
+  hasGradient: false,
+  topGradient: "#4B75FF",
+  botGradient: "#ffffff", //87CFE6
+
+  resolution: 2,
+  totalTime: 26,
+  hasSound: false,
+
+  gotoMarketClickNum: 0, ///
+  gotoMarketAfterTime: 0, ///
+
+  gotoMarketOnSecondLevel: false,
 };
 
-
-if(window.dashboardParams){
-    for(var prop in dashboardParams){
-        if(dashboardParams.hasOwnProperty(prop)){
-            data[prop]=dashboardParams[prop];
-        }
+if (window.dashboardParams) {
+  for (var prop in dashboardParams) {
+    if (dashboardParams.hasOwnProperty(prop)) {
+      data[prop] = dashboardParams[prop];
     }
+  }
 }
-
 
 data.gotoMarketClickNum = Number(data.gotoMarketClickNum);
 data.gotoMarketAfterTime = Number(data.gotoMarketAfterTime);
 app.curGoToMarketClickNum = 0;
 
-
-if(data.endCardNo == 1){
-    data.gotoMarketOnSecondLevel = true;
-    data.totalTime = 0;
+if (data.endCardNo == 1) {
+  data.gotoMarketOnSecondLevel = true;
+  data.totalTime = 0;
 }
 
-
-app.data=data;
+app.data = data;
 
 ///meaning THREE is ready
-function boot(){
-    game.boot(main);
+function boot() {
+  game.boot(main);
 }
 
-function init(){
-    
-    game.init();
-    
-    /*
+function init() {
+  game.init();
+
+  /*
     if(app.type!= "mobvista" && data.musicSrc){
         app.music = new Howl({
             src: [data.musicSrc],
@@ -124,123 +115,106 @@ function init(){
         });
     }
     */
-    
-    function checkMarket(e){
-        
-        if(app.gotoMarketAtNextClick){
-            main.gotoLink();
-            return;
-        }
-        app.curGoToMarketClickNum++;
-        
-        if(data.gotoMarketClickNum && app.curGoToMarketClickNum>=data.gotoMarketClickNum){
-            main.gotoLink();
-            return;
-        }
-        
-        if(data.gotoMarketAfterTime && main.time.passed()>data.gotoMarketAfterTime){
-            main.gotoLink();
-        }
-        
-        
+
+  function checkMarket(e) {
+    if (app.gotoMarketAtNextClick) {
+      main.gotoLink();
+      return;
     }
-    
-    document.body.addEventListener("touchstart",checkMarket);
-    if ('ontouchstart' in document.documentElement) {
-        
+    app.curGoToMarketClickNum++;
+
+    if (data.gotoMarketClickNum && app.curGoToMarketClickNum >= data.gotoMarketClickNum) {
+      main.gotoLink();
+      return;
     }
-    else{
-        document.body.addEventListener("mousedown",checkMarket);
+
+    if (data.gotoMarketAfterTime && main.time.passed() > data.gotoMarketAfterTime) {
+      main.gotoLink();
     }
-    
+  }
+
+  document.body.addEventListener("touchstart", checkMarket);
+  if ("ontouchstart" in document.documentElement) {
+  } else {
+    document.body.addEventListener("mousedown", checkMarket);
+  }
 }
 
-function gameResized(w,h,cw,ch){
-    game.onResizeCallback(w,h,cw,ch);
+function gameResized(w, h, cw, ch) {
+  game.onResizeCallback(w, h, cw, ch);
 }
 
-function noWebGLcallback(){
-    
-}
-
+function noWebGLcallback() {}
 
 var config = {
-    totalTime: data.totalTime,
-    resolution: data.resolution || 2,//1.5
-    FOV: 40,
-    cameraFar: 18000,//1800
-    gameStartCallback: init,
-    resizeCallback: gameResized,
-    gameResized,
+  totalTime: data.totalTime,
+  resolution: data.resolution || 2, //1.5
+  FOV: 40,
+  cameraFar: 18000, //1800
+  gameStartCallback: init,
+  resizeCallback: gameResized,
+  gameResized,
 
-    nucleo:{title:"Demo Game", genre:"Hyper Casual", version:"9.0.0"},
-    
-    renderer:{
-        antialias: false,
-        alpha:true,///transparent
-        bgColor: data.bgColor,
-        gradient: data.hasGradient ? {top: data.topGradient, bot: data.botGradient, }:null,
-        //canvasId:"canvas"
-    },
+  nucleo: {title: "Demo Game", genre: "Hyper Casual", version: "9.0.0"},
+
+  renderer: {
+    antialias: false,
+    alpha: true, ///transparent
     bgColor: data.bgColor,
-    transparent: false,
+    gradient: data.hasGradient ? {top: data.topGradient, bot: data.botGradient} : null,
+    //canvasId:"canvas"
+  },
+  bgColor: data.bgColor,
+  transparent: false,
 
-    //addTwoPhysicsWorld:true,///for matter
-    gravity: {x:0,y:-10,z:0},
-    noWebGLcallback:noWebGLcallback,
-    threeReady:boot,
-    
-    gamePaused: function(){
-        //Howler.mute(true);
-    },
-    gameContinue: function(){
-        //Howler.mute(false);
-    },
+  //addTwoPhysicsWorld:true,///for matter
+  gravity: {x: 0, y: -10, z: 0},
+  noWebGLcallback: noWebGLcallback,
+  threeReady: boot,
 
-    timeUp: function(){
-        game.endGame(false, "timeup");
-    },
+  gamePaused: function () {
+    //Howler.mute(true);
+  },
+  gameContinue: function () {
+    //Howler.mute(false);
+  },
 
-    soundChanged: function(){
-        
-        if(main.soundEnabled){
-            //Howler.mute(false);
-        }
-        else{
-            //Howler.mute(true);
-        }
+  timeUp: function () {
+    game.endGame(false, "timeup");
+  },
+
+  soundChanged: function () {
+    if (main.soundEnabled) {
+      //Howler.mute(false);
+    } else {
+      //Howler.mute(true);
     }
-}
+  },
+};
 
-if(!window.NUC){
-    config.nucleo = null;
+if (!window.NUC) {
+  config.nucleo = null;
 }
 
 var main = new Main(config);
 app.main = main;
 
+if (app.type == "mobvista") {
+  app.gameStart = function () {
+    main.startTimer();
+    main.timeOfStart = new Date().getTime();
 
-if(app.type == "mobvista"){
-    
-    app.gameStart = function()
-    {
-        
-        main.startTimer();
-        main.timeOfStart = new Date().getTime();
-        
-        if(data.musicSrc){
-            app.music = new Howl({
-                src: [data.musicSrc],
-                autoplay: true,
-                loop:true
-            });
-        }
-	}
-    
-    app.gameClose = function()
-    {
-        if(!app.music)return;
-        app.music.stop();
-	}
+    if (data.musicSrc) {
+      app.music = new Howl({
+        src: [data.musicSrc],
+        autoplay: true,
+        loop: true,
+      });
+    }
+  };
+
+  app.gameClose = function () {
+    if (!app.music) return;
+    app.music.stop();
+  };
 }
-
